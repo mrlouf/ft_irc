978930f933e906387806b31c75436505b19e5c15
nponchon <nponchon@student.42.fr>
2025-03-27 15:37:21 +0100

    so I guess we have a bot then

--- src/ServerManager.cpp
+++ src/ServerManager.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ServerManager.cpp                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hmunoz-g <hmunoz-g@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nponchon <nponchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 17:08:39 by hmunoz-g          #+#    #+#             */
-/*   Updated: 2025/03/27 13:54:21 by hmunoz-g         ###   ########.fr       */
+/*   Updated: 2025/03/27 15:35:03 by nponchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -115,7 +115,7 @@ void ServerManager::run() {
 			for (std::map<std::string, Channel>::const_iterator it = _channelManager->getChannelList().begin(); it != _channelManager->getChannelList().end(); ++it) {
 					
 					RegisteredClient* bot = _clientManager->getClientFromFd(1000);
-					std::string message = "PRIVMSG " + it->second.getName() + " : holiwi";
+					std::string message = "PRIVMSG " + it->second.getName() + " : **you are currently evaluating a 125/125 ft_irc**";
 					_commandManager->executeCommand(bot->getFd(), message);
 				}
 			lastBotMessageTime = currentTime;
